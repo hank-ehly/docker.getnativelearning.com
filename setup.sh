d86369d2b00cb04c6141fbb82418e406346db4e9
Hank Ehly <henry.ehly@gmail.com>
2017-08-28 12:08:39 +0900

    Initial commit

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+git clone git@github.com:hank-ehly/getnativelearning.com.git
+git clone git@github.com:hank-ehly/api.getnativelearning.com.git
+git clone git@github.com:hank-ehly/admin.getnativelearning.com.git
+
