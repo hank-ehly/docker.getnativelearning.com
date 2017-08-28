a9dab7a1765d819e694b902b7575ef907161b004
Hank Ehly <henry.ehly@gmail.com>
2017-08-28 12:15:15 +0900

    Add logging to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,20 @@
 #!/usr/bin/env bash
 
-git clone git@github.com:hank-ehly/getnativelearning.com.git
-git clone git@github.com:hank-ehly/api.getnativelearning.com.git
-git clone git@github.com:hank-ehly/admin.getnativelearning.com.git
+if [ ! -d getnativelearning.com ]; then
+    git clone git@github.com:hank-ehly/getnativelearning.com.git
+else
+    echo "getnativelearning.com already exists. Ignoring."
+fi
+
+if [ ! -d api.getnativelearning.com ]; then
+    git clone git@github.com:hank-ehly/api.getnativelearning.com.git
+else
+    echo "api.getnativelearning.com already exists. Ignoring."
+fi
+
+if [ ! -d admin.getnativelearning.com ]; then
+    git clone git@github.com:hank-ehly/admin.getnativelearning.com.git
+else
+    echo "admin.getnativelearning.com already exists. Ignoring."
+fi
 
